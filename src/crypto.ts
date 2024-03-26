d9563c150a38410d6ad233c9bade52bf5ccf1e29
Jerooome <jeromehenin@hotmail.fr>
2024-03-26 22:21:23 +0100

    travail accompli

---
+++ src/crypto.ts
@@ -0,0 +1,181 @@
+import { webcrypto } from "crypto";
+
+// #############
+// ### Utils ###
+// #############
+
+// Function to convert ArrayBuffer to Base64 string
+function arrayBufferToBase64(buffer: ArrayBuffer): string {
+  return Buffer.from(buffer).toString("base64");
+}
+
+// Function to convert Base64 string to ArrayBuffer
+function base64ToArrayBuffer(base64: string): ArrayBuffer {
+  var buff = Buffer.from(base64, "base64");
+  return buff.buffer.slice(buff.byteOffset, buff.byteOffset + buff.byteLength);
+}
+
+// ################
+// ### RSA keys ###
+// ################
+
+// Generates a pair of private / public RSA keys
+type GenerateRsaKeyPair = {
+  publicKey: webcrypto.CryptoKey;
+  privateKey: webcrypto.CryptoKey;
+};
+export async function generateRsaKeyPair(): Promise<GenerateRsaKeyPair> {
+  const { publicKey, privateKey } = await webcrypto.subtle.generateKey(
+    {
+      name: "RSA-OAEP",
+      modulusLength: 2048, // Can be 1024, 2048, or 4096
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: "SHA-256",
+    },
+    true, // whether the key is extractable (i.e., can be used in exportKey)
+    ["encrypt", "decrypt"]
+  );
+  return { publicKey, privateKey };
+}
+
+export async function exportPubKey(key: webcrypto.CryptoKey): Promise<string> {
+  const exportedKey = await webcrypto.subtle.exportKey("spki", key);
+  return arrayBufferToBase64(exportedKey);
+}
+
+export async function exportPrvKey(key: webcrypto.CryptoKey | null): Promise<string | null> {
+  try {
+    if (key === null) {return null;}
+    else {
+      const exportedPrivKey = await crypto.subtle.exportKey('pkcs8', key);
+      const privKeyBase64Key = arrayBufferToBase64(exportedPrivKey);
+      return privKeyBase64Key;
+    }
+  } catch (err) {
+    console.error("Error exporting private key:", err);
+    throw err;
+  }
+}
+
+// Import a base64 string public key to its native format
+export async function importPubKey(strKey: string): Promise<webcrypto.CryptoKey> {
+  const keyBuffer = base64ToArrayBuffer(strKey);
+  return webcrypto.subtle.importKey(
+    "spki",
+    keyBuffer,
+    {
+      name: "RSA-OAEP",
+      hash: "SHA-256",
+    },
+    true,
+    ["encrypt"]
+  );
+}
+
+export async function importPrvKey(strKey: string): Promise<webcrypto.CryptoKey> {
+  const keyBuffer = base64ToArrayBuffer(strKey);
+  return webcrypto.subtle.importKey(
+    "pkcs8",
+    keyBuffer,
+    {
+      name: "RSA-OAEP",
+      hash: "SHA-256",
+    },
+    true,
+    ["decrypt"]
+  );
+}
+
+// Encrypt a message using an RSA public key
+export async function rsaEncrypt(b64Data: string, strPublicKey: string): Promise<string> {
+  const publicKey = await importPubKey(strPublicKey);
+  const dataBuffer = base64ToArrayBuffer(b64Data);
+  const encryptedData = await webcrypto.subtle.encrypt(
+    { name: "RSA-OAEP" },
+    publicKey,
+    dataBuffer
+  );
+  return arrayBufferToBase64(encryptedData);
+}
+
+export async function rsaDecrypt(data: string, privateKey: webcrypto.CryptoKey): Promise<string> {
+  const dataBuffer = base64ToArrayBuffer(data);
+  const decryptedData = await webcrypto.subtle.decrypt(
+    { name: "RSA-OAEP" },
+    privateKey,
+    dataBuffer
+  );
+  return new TextDecoder().decode(decryptedData);
+}
+
+
+
+// ######################
+// ### Symmetric keys ###
+// ######################
+
+// Generates a random symmetric key
+export async function createRandomSymmetricKey(): Promise<webcrypto.CryptoKey> {
+  return webcrypto.subtle.generateKey(
+    {
+      name: "AES-GCM",
+      length: 256,
+    },
+    true,
+    ["encrypt", "decrypt"]
+  );
+}
+
+export async function exportSymKey(key: webcrypto.CryptoKey): Promise<string> {
+  const exportedKey = await webcrypto.subtle.exportKey("raw", key);
+  return arrayBufferToBase64(exportedKey);
+}
+
+export async function importSymKey(strKey: string): Promise<webcrypto.CryptoKey> {
+  const keyBuffer = base64ToArrayBuffer(strKey);
+  return webcrypto.subtle.importKey(
+    "raw",
+    keyBuffer,
+    "AES-GCM",
+    true,
+    ["encrypt", "decrypt"]
+  );
+}
+
+export async function symEncrypt(key: webcrypto.CryptoKey, data: string): Promise<string> {
+  const encodedData = new TextEncoder().encode(data);
+  const encryptedData = await webcrypto.subtle.encrypt(
+    {
+      name: "AES-GCM",
+      iv: new TextEncoder().encode("A unique IV"), // IV should be unique for each encryption
+    },
+    key,
+    encodedData
+  );
+  return arrayBufferToBase64(encryptedData);
+}
+
+
+// Decrypt a message using a symmetric key
+export async function symDecrypt(strKey: string,encryptedData: string): Promise<string> {
+  try {
+    const encryptedDataWithIV = base64ToArrayBuffer(encryptedData);
+    const iv = new Uint8Array(encryptedDataWithIV.slice(0, 16));
+    const encryptedDataBuffer = encryptedDataWithIV.slice(16);
+    const simKey = await importSymKey(strKey);
+    const decryptedData = await crypto.subtle.decrypt(
+      {
+        name: 'AES-CBC',
+        iv: iv,
+      },
+      simKey,
+      encryptedDataBuffer
+    );
+    const decryptedText = new TextDecoder().decode(decryptedData);
+    return decryptedText;
+  }
+  catch (err) {
+    console.error("Error decrypting data:", err);
+    throw err;
+  }
+}
---
+++ src/registry/registry.ts
@@ -0,0 +1,62 @@
+import bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+import { REGISTRY_PORT } from "../config";
+
+export type Node = { nodeId: number; pubKey: string };
+
+export type RegisterNodeBody = {
+  nodeId: number;
+  pubKey: string;
+};
+
+export type GetNodeRegistryBody = {
+  nodes: Node[];
+};
+
+export async function launchRegistry() {
+  const _registry = express();
+  _registry.use(express.json());
+  _registry.use(bodyParser.json());
+
+  // TODO implement the status route
+  // _registry.get("/status", (req, res) => {});
+
+  _registry.get("/status", (req, res) => {
+    res.send('live');
+  });
+
+  
+  let nodes: Node[] = [];
+
+  _registry.post('/registerNode', (req, res) => {
+    const {nodeId, pubKey} = req.body;
+    if (!nodeId || !pubKey) {
+      res.status(400).send("Invalid request body");
+      return;
+    }
+    const nodeExists = nodes.some((node) => node.nodeId === nodeId);
+    const pubKeyExists = nodes.some((node) => node.pubKey === pubKey);
+    console.log(`Registering node: ${nodeId}`);
+
+    if (nodeExists) {
+      res.status(400).send("Node already exists");
+    } else if (pubKeyExists) {
+      res.status(400).send("Public key already exists");
+    } else {
+      nodes.push({nodeId, pubKey});
+      res.status(200).send("Node registered successfully");
+    }
+  });
+
+  // Get the node registry
+  _registry.get("/getNodeRegistry", (req, res) => {
+    const registry: GetNodeRegistryBody = { nodes };
+    res.status(200).json(registry);
+  });
+
+  const server = _registry.listen(REGISTRY_PORT, () => {
+    console.log(`registry is listening on port ${REGISTRY_PORT}`);
+  });
+
+  return server;
+}
